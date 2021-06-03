031321bd9f1374aa048dc09d200563c1ad92e080
Boomb0x <Ghost2130@gmail.com>
2021-06-02 21:52:19 -0400

    added comments and the README

---
+++ README.md
@@ -0,0 +1,15 @@
+# Homework-4 (Work Day Planner)
+## Links
+
+[Repository](https://github.com/Aladd616/Work-Day-Scheduler-ALL)
+
+[Pages](https://aladd616.github.io/Work-Day-Scheduler-ALL/)
+
+## Description
+
+A webpage that shows a series of rows labeled with the 8 business hours.  the text fields are colored based upon what the current hour of the day is.  When text is entered into the text field and the save button is pressed it saves that text and displays it upon the page reloading.
+
+The color of the text fields is determined by comparing the value of the id attribute associated with each text field to the current hour stored in the variable hour, grey for past, red for current hour, and green for future.
+
+Upon pressing the save button the tet within the text area is saved within local storage labeled with the id attribute associated with the text field.  Upon page load this data is displayed within the text field labeled with the id attribute.
+
--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+// save button variable
 var saveBtn = $('.saveBtn');
 
+// moment to display the current day in the header
 var weekDay = moment().format('dddd, MMMM Do');
 $("#currentDay").text(weekDay);
 
+// variable for the current hour
 var hour = Number(moment().format('H'));
 console.log("hour", hour);
 
+// creates array of the description elements of the page
 const descriptions = $('.description');
+// empty array for id elements
 var idArray = [];
     
+// checks if there is data in local storage and projects it into the correct text field
     $('.description').each(function() {
         var id = $(this).parent().attr('id');
         idArray.push(id);
@@ -23,7 +29,7 @@ var idArray = [];
     });
 
 
-
+// places colors on the text fields of each hour using the variable hour
 function getLineClass(h) {
     if (h < idArray) {
         return 'past';
@@ -45,7 +51,7 @@ for (i = 0; i < descriptions.length; i++) {
 }
 
 
-
+// when the save button is clicked saves what is in the textfield to locastorage labled by the of of the section
 saveBtn.on('click', function() {
     var text = $(this).siblings('.description').val();
     var hourly = $(this).parent().attr('id');
